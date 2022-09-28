e6b88efe63f75636ec6567b16388c90f341d39fb
Francesco <francesco.giancipoli@glue-labs.com>
2022-09-28 18:28:26 +0200

    fix

--- lib/widgets/personal_header.dart
+++ lib/widgets/personal_header.dart
@@ -32,105 +32,100 @@ class _PersonalHeaderState extends State<PersonalHeader>
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(top: 30),
-      child: !isItInLibrary
-          ? Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.transparent,
-                        shadowColor: Colors.transparent,
-                        minimumSize: Size(0, 0),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                        elevation: 0,
-                        // shadowColor: Colors.transparent
-                      ),
-                      onPressed: goToMyHome,
-                      child: Image.asset(
-                        'assets/left-arrow-icon.jpeg',
-                        height: 30,
-                        width: 30,
-                      )),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    child: Text(
-                      'My Profile',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    margin: const EdgeInsets.only(left: 68),
-                  ),
-                ),
-                // Container(
-                //   width: 100,
-                // ),
-                Expanded(
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.transparent,
-                          shadowColor: Colors.transparent,
-                          minimumSize: Size(0, 0),
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                          elevation: 0,
-                        ),
-                        onPressed: () {
-                          if (_controller.isDismissed) {
-                            _controller.forward();
-                            openSettings(_controller, _duration);
-                          } else if (_controller.isCompleted)
-                            _controller.reverse();
-                        },
-                        child: RotationTransition(
-                          turns: const AlwaysStoppedAnimation(90 / 360),
-                          child: Image.asset(
-                            'assets/more-512.webp',
-                            height: 30,
-                            width: 30,
-                          ),
-                        )))
-              ],
-            )
-          : Row(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(left: 10),
-                  child: Expanded(
-                    flex: 1,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.transparent,
-                          shadowColor: Colors.transparent,
-                          minimumSize: Size(0, 0),
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                          elevation: 0,
-                          // shadowColor: Colors.transparent
-                        ),
-                        onPressed: goToMyHome,
-                        child: Image.asset(
-                          'assets/left-arrow-icon.jpeg',
-                          height: 30,
-                          width: 30,
-                        )),
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    margin: const EdgeInsets.only(left: 100),
-                    child: Text(
-                      'My Library',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                  ),
-                ),
-                Text('')
-              ],
+      child: !isItInLibrary ? profileHeader() : libraryHeader(),
+    );
+  }
+
+  Row profileHeader() {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.transparent,
+                shadowColor: Colors.transparent,
+                minimumSize: Size(0, 0),
+                padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                elevation: 0,
+                // shadowColor: Colors.transparent
+              ),
+              onPressed: goToMyHome,
+              child: Image.asset(
+                'assets/left-arrow-icon.jpeg',
+                height: 30,
+                width: 30,
+              )),
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            child: Text(
+              'My Profile',
+              style: TextStyle(fontSize: 15),
             ),
+            margin: const EdgeInsets.only(left: 68),
+          ),
+        ),
+        // Container(
+        //   width: 100,
+        // ),
+        Expanded(
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                  minimumSize: Size(0, 0),
+                  padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                  elevation: 0,
+                ),
+                onPressed: () {
+                  if (_controller.isDismissed) {
+                    _controller.forward();
+                    openSettings(_controller, _duration);
+                  } else if (_controller.isCompleted) _controller.reverse();
+                },
+                child: RotationTransition(
+                  turns: const AlwaysStoppedAnimation(90 / 360),
+                  child: Image.asset(
+                    'assets/more-512.webp',
+                    height: 30,
+                    width: 30,
+                  ),
+                )))
+      ],
+    );
+  }
+
+  Row libraryHeader() {
+    return Row(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(left: 10),
+          child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.transparent,
+                shadowColor: Colors.transparent,
+                minimumSize: Size(0, 0),
+                padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                elevation: 0,
+                // shadowColor: Colors.transparent
+              ),
+              onPressed: goToMyHome,
+              child: Image.asset(
+                'assets/left-arrow-icon.jpeg',
+                height: 30,
+                width: 30,
+              )),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 100),
+          child: Text(
+            'My Library',
+            style: TextStyle(fontSize: 15),
+          ),
+        )
+      ],
     );
   }
 }
